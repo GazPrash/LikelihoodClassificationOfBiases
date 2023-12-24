d76c0cc49a48a9bcf8bcbd4bfcb3e22d9e83b2f7
pShr <prashantshrivastava118@gmail.com>
2023-12-24 20:01:06 +0530

    Added Ensemble Th Method

--- src/model.py
+++ src/model.py
@@ -5,20 +5,28 @@ from tqdm import trange
 
 
 class LikelihoodModelForNormalDist:
-    def __init__(self, t1, t2, threshold_limit = 0) -> None:
+    def __init__(self, t1, t2, threshold_limit=0) -> None:
         self.t1 = t1
         self.t2 = t2
+        self.thresholds = []
         self.threshold_limit = threshold_limit
 
         self.U1 = None
         self.U2 = None
         self.Un = None
 
+    def ensemble_thresholds():
+        # process for generating thresholds
+        for t1, t2 in self.thresholds:
+            return self.fit_total_pop() # SOMETHING like this
+
     def fit_total_pop(self, total_pop):
         sorted_data = np.sort(total_pop)
         quantiles = np.quantile(sorted_data, [self.t1, self.t2])
         self.U2 = sorted_data[sorted_data <= quantiles[0]]
-        self.Un = sorted_data[(sorted_data > quantiles[0]) & (sorted_data < quantiles[1])]
+        self.Un = sorted_data[
+            (sorted_data > quantiles[0]) & (sorted_data < quantiles[1])
+        ]
         self.U1 = sorted_data[sorted_data > quantiles[1]]
         # self.U2 = sorted_data[sorted_data <= self.t1]
         # self.Un = sorted_data[(sorted_data > self.t1) & (sorted_data < self.t2)]
@@ -35,9 +43,15 @@ class LikelihoodModelForNormalDist:
         for ind in trange(len(word_biases)):
             sample = word_biases[ind]
 
-            likelihood1 = np.sum((norm.logpdf(sample, np.mean(self.U1), np.std(self.U1))))
-            likelihood2 = np.sum((norm.logpdf(sample, np.mean(self.U2), np.std(self.U2))))
-            likelihood3 = np.sum((norm.logpdf(sample, np.mean(self.Un), np.std(self.Un))))
+            likelihood1 = np.sum(
+                (norm.logpdf(sample, np.mean(self.U1), np.std(self.U1)))
+            )
+            likelihood2 = np.sum(
+                (norm.logpdf(sample, np.mean(self.U2), np.std(self.U2)))
+            )
+            likelihood3 = np.sum(
+                (norm.logpdf(sample, np.mean(self.Un), np.std(self.Un)))
+            )
 
             all_likelihoods = [likelihood1, likelihood2, likelihood3]
             best_fit_population = np.argmax(all_likelihoods)
@@ -50,7 +64,7 @@ class LikelihoodModelForNormalDist:
             predictions.append(best_fit_population)
             if best_fit_population == 2:
                 neutral.append((ind, all_likelihoods))
-            elif best_fit_population == 0 and max(all_likelihoods) :
+            elif best_fit_population == 0 and max(all_likelihoods):
                 c1_biased.append((ind, all_likelihoods))
             else:
                 c2_biased.append((ind, all_likelihoods))
